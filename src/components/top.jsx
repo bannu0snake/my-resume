e2fce07bcd00f2abd668e2ecda7469de9790853f
Mahaboob Shaik <mahaboob.developer@gmail.com>
2021-09-22 03:40:16 +0530

    updated top

--- src/components/top.jsx
+++ src/components/top.jsx
@@ -9,17 +9,17 @@ function Top(params) {
         
           <img src="images/mahaboob.jpg"  class="img-rounded profile-pic" alt="Cinque Terre"/>
           <h1 class="text-uppercase">MAHABOOB SHAIK</h1>
-          <p>FULL STACK DEVELOPER | DATA ANALYST | FLUTTER DEVELOPER | PYTHON DEVELOPER</p>
+          <p>FULL STACK DEVELOPER | FLUTTER DEVELOPER | PYTHON DEVELOPER | MACHINE LEARNING</p>
          
         
         </div>
         <div class="col-lg-6 col-md-6 content-item content-item-1 background tm-white-translucent-bg">
           <h2 class="main-title text-center dark-blue-text">About Me</h2>
           <p>
-            I am currently pursuing my II year B Tech. at <strong>IIIT, Sri City</strong>  in <strong>Computer Science</strong> . I am very much passionate in Data Scince 
-            and Full Stack Development and also I am enthusiastic in participating coding challages and 
-            helping my teammates during challages.
-            My hobbies are playing Cricket and participating in coding challages.    
+            I am currently pursuing my III year B Tech. at <strong>IIIT, Sri City</strong>  in <strong>Computer Science</strong> . I am
+            Full Stack Web Developer , Flutter App Developer and also Python developer. I am  enthusiastic in participating coding challages and 
+            I am always been a good team member.
+            My hobbies are playing Cricket, Ethical Hacking and participating in coding challages.    
           </p>
         </div>
       </section>
